8befc5d81a38783cff2c94730e834514e78fc93d
Mion giovanni <mion.ggb@gmail.com>
2021-12-13 01:01:24 +0100

    add friend removal message

---
+++ src/main/kotlin/com.bubul.col.messages/friend/FriendRemovalMsg.kt
@@ -0,0 +1,31 @@
+package com.bubul.col.messages.friend
+
+import com.bubul.col.messages.MqttMessage
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+@Serializable
+data class FriendRemovalMsgPayload(val source : String, val target : String)
+
+class FriendRemovalMsg(val source : String, val target : String) : MqttMessage() {
+
+    override fun serialize(): ByteArray {
+        val payload = Json.encodeToString(FriendRemovalMsgPayload(source,target))
+        return payload.toByteArray()
+    }
+
+    override fun getMqttTopic(): String {
+        return topic
+    }
+
+    companion object {
+        const val topic = "${MqttMessage.topic}/FriendService/FriendRemoval"
+
+        fun deserialize(data : ByteArray) : FriendRemovalMsg {
+            val payload = Json.decodeFromString<FriendRemovalMsgPayload>(String(data))
+            return FriendRemovalMsg(payload.source,payload.target)
+        }
+    }
+}
