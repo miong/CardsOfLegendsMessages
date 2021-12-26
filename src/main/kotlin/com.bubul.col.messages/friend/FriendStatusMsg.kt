27339b2c2b0656487b58ef95fe05f5d4927abe33
Mion giovanni <mion.ggb@gmail.com>
2021-12-26 16:24:41 +0100

    add friend status msg

---
+++ src/main/kotlin/com.bubul.col.messages/friend/FriendStatusMsg.kt
@@ -0,0 +1,32 @@
+package com.bubul.col.messages.friend
+
+import com.bubul.col.messages.MqttMessage
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import javax.swing.text.html.parser.Entity
+
+@Serializable
+data class FriendStatusMsgPayload(val source : String, val target : String, val status : FriendStatus, val sourceEntity: String)
+
+class FriendStatusMsg(val source : String, val target : String, val status : FriendStatus, val sourceEntity: String) : MqttMessage() {
+
+    override fun serialize(): ByteArray {
+        val payload = Json.encodeToString(FriendStatusMsgPayload(source,target,status,sourceEntity))
+        return payload.toByteArray()
+    }
+
+    override fun getMqttTopic(): String {
+        return topic
+    }
+
+    companion object {
+        const val topic = "${MqttMessage.topic}/FriendService/FriendStatus"
+
+        fun deserialize(data : ByteArray) : FriendStatusMsg {
+            val payload = Json.decodeFromString<FriendStatusMsgPayload>(String(data))
+            return FriendStatusMsg(payload.source,payload.target,payload.status,payload.sourceEntity)
+        }
+    }
+}
